5e4cf6352d9f3afdde96f663ae8f11dd5e7dce54
darkTeaLeaf <arina.fedorovskaya@gmail.com>
2019-03-18 01:25:55 +0300

    Offer model

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+
+class Offer(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    offer_description = models.TextField
+    published_date = models.DateTimeField(default=timezone.now)
+
+    def publish(self):
+        self.save()
