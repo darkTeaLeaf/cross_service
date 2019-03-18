5f68ebb8be224fcdfd4a65d6e778e8a25826aad0
reconrus <reconrus@yandex.ru>
2019-03-18 21:47:53 +0300

    Edit personal info page

--- user/forms.py
+++ user/forms.py
@@ -19,3 +19,36 @@ class CreateUserForm(UserCreationForm):
         # telegram_alias = self.cleaned_data['telegram_alias']
         if commit:
             user.save(True)
+
+class EditUserForm(forms.ModelForm):
+    username = forms.CharField(required=True)
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=False)
+    last_name = forms.CharField(required=False)
+    telegram_alias = forms.CharField(required=False)
+
+    class Meta:
+        fields = [
+            'username', 'first_name', 'last_name', 'email', 'telegram_alias'
+        ]
+        model = User
+
+    def clean_email(self):
+        username = self.cleaned_data.get('username')
+        email = self.cleaned_data.get('email')
+
+        if email and User.objects.filter(email=email).exclude(username=username).count():
+            raise forms.ValidationError('This email address is already in use. Please supply a different email address.')
+        return email
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.email = self.cleaned_data['email']
+        user.first_name = self.cleaned_data['first_name']
+        user.last_name = self.cleaned_data['lasst_name']
+        user.telegram_alias = self.cleaned_data['telegram_alias']
+
+        if commit:
+            user.save()
+
+        return user
