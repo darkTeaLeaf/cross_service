b2cde4721b272d6c732e802fdfb3be906bcd69ff
darkTeaLeaf <arina.fedorovskaya@gmail.com>
2019-03-19 11:08:33 +0300

    Magic code for fixing problem with statics

--- cross_service/settings.py
+++ cross_service/settings.py
@@ -120,8 +120,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
-STATIC_URL = BASE_DIR + '/static/'
-
+#STATIC_URL = BASE_DIR + '/static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Redirected here after a successful login
 LOGIN_REDIRECT_URL = '/'
