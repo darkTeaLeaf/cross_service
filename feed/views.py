acf00ecd5efbb163624ae3112e8274671ea8cc93
Danil Ginzburg <danilgin@inbox.ru>
2019-05-03 10:54:55 +0300

    new 404 page

--- feed/views.py
+++ feed/views.py
@@ -200,8 +200,10 @@ def create_respond_offer(request, id):
 
 
 def handler404(request, *args, **kwargs):
-    pass
+    response = render(request, 'feed/404.html')
+    response.status_code = 404
+    return response
 
 
-def handler500(request, *args, **kwargs):
-    pass
+# def handler500(request, *args, **kwargs):
+#     pass
